cc6b724f03207faee34795a7b686f7906130894d
xinyiaw <xinyiaw@live.com.sg>
2021-10-30 14:01:10 +0800

    Auto Rotate Image if needed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { createWorker } from 'tesseract.js';
 
 @Component({
@@ -9,19 +9,68 @@ import { createWorker } from 'tesseract.js';
 export class AppComponent {
   title = 'tesseract.js-angular-app';
   ocrResult = 'Recognizing...';
-  constructor() {
-    this.doOCR();
+  imageUpload: any;
+  imgSrc: any;
+  confidence: any;
+
+  constructor(private renderer: Renderer2) {
+
   }
   async doOCR() {
+
     const worker = createWorker({
       logger: m => console.log(m),
     });
+
     await worker.load();
     await worker.loadLanguage('eng');
     await worker.initialize('eng');
-    const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+
+
+    const { data: { text, confidence } } = await worker.recognize(this.imgSrc);
     this.ocrResult = text;
-    console.log(text);
+    this.confidence = confidence;
+    console.log(this.ocrResult)
     await worker.terminate();
+
+    //if confidence < 80m, flip image
+    if (confidence < 80) {
+      this.rotateImage();
+    }
+
+  }
+
+  rotateImage() {
+    console.log('-----')
+    console.log('rotating image --- ')
+    let img = new Image();
+    img.src = this.imgSrc;
+
+    var canvas = document.createElement('canvas')
+    canvas.width = img.height
+    canvas.height = img.width
+    canvas.style.position = "absolute"
+    var ctx = canvas.getContext("2d")
+    ctx.imageSmoothingEnabled = false
+    ctx.translate(img.height, img.width / img.height)
+    ctx.rotate(Math.PI / 2)
+    ctx.drawImage(img, 0, 0)
+
+    this.imgSrc = canvas.toDataURL("image/png");
+    this.doOCR();
+  }
+
+  readImage(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        this.imgSrc = event.target.result;
+      }
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
+
+    this.doOCR()
   }
 }
